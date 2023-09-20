dc12d08aa3375465bbff24160d01dc88ca5d403a
Krisztian Nagy <chrisakakay@gmail.com>
2023-09-20 13:35:47 +0200

    basic relationships

--- spec/Serializeable.test.js
+++ spec/Serializeable.test.js
@@ -22,7 +22,7 @@ test('using simple class', t => {
     included: []
   };
 
-  t.is(JSON.stringify(expected), car.serialize());
+  t.is(JSON.stringify(expected), car.serialize().toJSON());
 });
 
 test('using simple class with more attributess', t => {
@@ -34,14 +34,14 @@ test('using simple class with more attributess', t => {
       ]
     }
 
-    constructor(id, name) {
+    constructor({ id, name }) {
       super();
       this.id = id;
       this.name = name;
     }
   };
 
-  const car = new Car(2, 'BMW');
+  const car = new Car({ id: 2, name: 'BMW' });
   const expected =   {
     data: {
       id: 2,
@@ -54,7 +54,7 @@ test('using simple class with more attributess', t => {
     included: []
   };
 
-  t.is(JSON.stringify(expected), car.serialize());
+  t.is(JSON.stringify(expected), car.serialize().toJSON());
 });
 
 test('using simple class with function true', t => {
@@ -67,7 +67,7 @@ test('using simple class with function true', t => {
       ]
     }
 
-    constructor(id, name, roles) {
+    constructor({ id, name, roles }) {
       super();
       this.id = id;
       this.name = name;
@@ -79,7 +79,7 @@ test('using simple class with function true', t => {
     }
   };
 
-  const user = new User(2, 'Krisz', ['ADMIN', 'USER']);
+  const user = new User({ id: 2, name: 'Krisz', roles: ['ADMIN', 'USER'] });
   const expected =   {
     data: {
       id: 2,
@@ -93,7 +93,7 @@ test('using simple class with function true', t => {
     included: []
   };
 
-  t.is(JSON.stringify(expected), user.serialize());
+  t.is(JSON.stringify(expected), user.serialize().toJSON());
 });
 
 test('using simple class with function false', t => {
@@ -106,7 +106,7 @@ test('using simple class with function false', t => {
       ]
     }
 
-    constructor(id, name, roles) {
+    constructor({ id, name, roles }) {
       super();
       this.id = id;
       this.name = name;
@@ -118,7 +118,7 @@ test('using simple class with function false', t => {
     }
   };
 
-  const user = new User(2, 'Krisz', ['USER']);
+  const user = new User({ id: 2, name: 'Krisz', roles: ['USER'] });
   const expected =   {
     data: {
       id: 2,
@@ -132,12 +132,12 @@ test('using simple class with function false', t => {
     included: []
   };
 
-  t.is(JSON.stringify(expected), user.serialize());
+  t.is(JSON.stringify(expected), user.serialize().toJSON());
 });
 
 test('using simple class with function and objects as attributes', t => {
   class User extends Serializeable {
-    type = 'user';
+    type = 'users';
     serializerConfig = {
       attributes: [
         { name: 'name' },
@@ -145,7 +145,7 @@ test('using simple class with function and objects as attributes', t => {
       ]
     }
 
-    constructor(id, name, roles) {
+    constructor({ id, name, roles }) {
       super();
       this.id = id;
       this.name = name;
@@ -157,11 +157,11 @@ test('using simple class with function and objects as attributes', t => {
     }
   };
 
-  const user = new User(2, 'Krisz', ['USER']);
+  const user = new User({ id: 2, name: 'Krisz', roles: ['USER'] });
   const expected =   {
     data: {
       id: 2,
-      type: 'user',
+      type: 'users',
       attributes: {
         name: 'Krisz',
         admin: false
@@ -171,5 +171,143 @@ test('using simple class with function and objects as attributes', t => {
     included: []
   };
 
-  t.is(JSON.stringify(expected), user.serialize());
+  t.is(JSON.stringify(expected), user.serialize().toJSON());
+});
+
+test('using simple class with relationships', t => {
+  class User extends Serializeable {
+    type = 'users';
+    serializerConfig = {
+      attributes: [
+        { name: 'name' },
+        { name: 'admin', value: (t) => t.isAdmin() }
+      ],
+      relationships: [
+        'roles'
+      ]
+    }
+
+    constructor({ id, name }) {
+      super();
+      this.id = id;
+      this.name = name;
+      this.roles = [];
+    }
+
+    isAdmin() {
+      return this.roles.findIndex(r => r.title === 'ADMIN') > -1;
+    }
+  };
+
+  class Role extends Serializeable {
+    type = 'roles';
+    serializerConfig = {
+      attributes: [
+        { name: 'title' },
+      ]
+    }
+
+    constructor({ id, title }) {
+      super();
+      this.id = id;
+      this.title = title;
+    }
+  }
+
+  const user = new User({ id: 2, name: 'Krisz' });
+  user.roles = [
+    new Role({ id: 5, title: 'SUPERUSER' }),
+    new Role({ id: 6, title: 'USER' })
+  ];
+
+  const expected =   {
+    data: {
+      id: 2,
+      type: 'users',
+      attributes: { name: 'Krisz', admin: false },
+      relationships: {
+        roles: {
+          data: [
+            { id: 5, type: 'roles' },
+            { id: 6, type: 'roles' }
+          ]
+        }
+      }
+    },
+    included: [
+      { id: 5, type: 'roles', attributes: { title: 'SUPERUSER' }, relationships: {} },
+      { id: 6, type: 'roles', attributes: { title: 'USER' }, relationships: {} },
+    ]
+  };
+
+  t.is(JSON.stringify(expected), user.serialize().toJSON());
+});
+
+test('using simple class with relationships 2', t => {
+  class User extends Serializeable {
+    type = 'users';
+    serializerConfig = {
+      attributes: [
+        { name: 'name' },
+        { name: 'admin', value: (t) => t.isAdmin() }
+      ],
+      relationships: [
+        'roles'
+      ]
+    }
+
+    constructor({ id, name }) {
+      super();
+      this.id = id;
+      this.name = name;
+      this.roles = [];
+    }
+
+    isAdmin() {
+      return this.roles.findIndex(r => r.title === 'ADMIN') > -1;
+    }
+  };
+
+  class Role extends Serializeable {
+    type = 'roles';
+    serializerConfig = {
+      attributes: [
+        { name: 'title' },
+      ]
+    }
+
+    constructor({ id, title }) {
+      super();
+      this.id = id;
+      this.title = title;
+    }
+  }
+
+  const user = new User({ id: 2, name: 'Krisz' });
+  user.roles = [
+    new Role({ id: 5, title: 'SUPERUSER' }),
+    new Role({ id: 6, title: 'ADMIN' })
+  ];
+
+  const expected =   {
+    data: {
+      id: 2,
+      type: 'users',
+      attributes: { name: 'Krisz', admin: true },
+      relationships: {
+        roles: {
+          data: [
+            { id: 5, type: 'roles' },
+            { id: 6, type: 'roles' }
+          ]
+        }
+      }
+    },
+    included: [
+      { id: 5, type: 'roles', attributes: { title: 'SUPERUSER' }, relationships: {} },
+      { id: 6, type: 'roles', attributes: { title: 'ADMIN' }, relationships: {} },
+    ]
+  };
+
+  t.is(JSON.stringify(expected), user.serialize().toJSON());
 });
